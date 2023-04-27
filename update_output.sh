ab588855933b997c4334c0cc495979cfab5cad0f
SylvainDe <sylvain.desodt+github@gmail.com>
2023-04-27 21:05:57 +0200

    Update/clean output files

---
+++ update_output.sh
@@ -0,0 +1,3 @@
+python main.py -t input_examples/list_of_urls_of_tabs.txt -o output/book
+python main.py -t input_examples/list_perso.txt -o output/tab_perso.html
+python main.py -l input_examples/list_of_urls_of_list.txt -o output/megabook
