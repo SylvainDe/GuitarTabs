aa1bf3746be6b251e8b5e4382cee08bb79c5e83f
SylvainDe <sylvain.desodt+github@gmail.com>
2023-07-30 13:01:18 +0200

    Add smaller example file relying on lists

--- update_output.sh
+++ update_output.sh
@@ -1,7 +1,9 @@
-python main.py -t input_examples/list_of_urls_of_tabs.txt -o output/book
-python main.py -t input_examples/list_perso.txt -o output/tab_perso.html
-python main.py -l input_examples/list_of_urls_of_list.txt -o output/megabook
+# python main.py -t input_examples/list_of_urls_of_tabs.txt -o output/book
+# python main.py -t input_examples/list_perso.txt -o output/tab_perso.html
+# python main.py -l input_examples/list_of_tops_tabs.txt -o output/toptabs
+# python main.py -l input_examples/list_of_urls_of_list.txt -o output/megabook
 
 python main.py -t input_examples/list_of_urls_of_tabs.txt -o output/book -f
 python main.py -t input_examples/list_perso.txt -o output/tab_perso.html -f
+python main.py -l input_examples/list_of_tops_tabs.txt -o output/toptabs -f
 python main.py -l input_examples/list_of_urls_of_list.txt -o output/megabook -f
